10a09f01be66237840fedbdebcbfcce30dd822ac
ohazyi <291045048@qq.com>
2018-08-04 16:50:14 +0800

    assignment1: svm

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -39,7 +39,12 @@ class LinearClassifier(object):
     for it in range(num_iters):
       X_batch = None
       y_batch = None
-
+      #print(dim*batch_size) #print(X.flatten().shape)
+      #X_batch = np.reshape(np.random.choice(X.flatten(), int(batch_size*dim), replace=True), (-1, dim)) #print(X_batch)
+      #y_batch = np.random.choice(y, batch_size, replace=True) #print(y_batch)
+      sample_index = np.random.choice(num_train, batch_size, replace=True) #从[0,num_train]中选取batch_size个数，replace为True代表里面的数可以重复
+      X_batch = X[sample_index, :]
+      y_batch = y[sample_index]
       #########################################################################
       # TODO:                                                                 #
       # Sample batch_size elements from the training data and their           #
@@ -65,7 +70,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W -= learning_rate*grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -89,12 +94,19 @@ class LinearClassifier(object):
       array of length N, and each element is an integer giving the predicted
       class.
     """
-    y_pred = np.zeros(X.shape[0])
+    # 预测出得分最高的那个label
+    y_pred = np.zeros(X.shape[0]) 
+    #print(y_pred.shape)
     ###########################################################################
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    #print("!!!!!!!!!!!")
+    scores = X.dot(self.W) #shape: (1000, 10) 1000个例子，每个class对应的得分 #计算出所有标签的得分
+    #print(scores)
+    #print(scores.shape)
+    y_pred = np.argmax(scores, axis = 1)
+    #print(y_pred)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -3,8 +3,10 @@ from random import shuffle
 
 def svm_loss_naive(W, X, y, reg):
   """
+  结构化的SVM代价函数，简单实现（有循环的）
   Structured SVM loss function, naive implementation (with loops).
 
+  输入有D维，C个种类，并且我们在N个样本中执行minibatch
   Inputs have dimension D, there are C classes, and we operate on minibatches
   of N examples.
 
@@ -22,6 +24,7 @@ def svm_loss_naive(W, X, y, reg):
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
   # compute the loss and the gradient
+  # 接下来就是计算SVM损失（这个定义可以看课程的视频，简单而言，就是如果W*X计算后的得分+1大于正确标签的得分（这个就相当于预测错了，即怎么可能比真实的标签值的得分还要大），那么就加上这个偏差，否则无视（我们可以这么理解，对于小的显然是应该的，所以就不应该加到损失函数里，否则就是加上偏差）
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
@@ -33,14 +36,19 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        dW[:, j] += X[i, :] #错误的标签的权重应当减少 #因为之后的SGD上有一个-，因此在这儿是减号 #至于为什么导数是X[i], 是因为由loss函数我们可以直观地看到导数项 #比如说j是5，即第五个分类，此时我们将权重矩阵中第5列的所有参数(3073个值)都加上X[i]这一行的值
+        dW[:, y[i]] -= X[i, :] #正确的标签的参数应当增加
         loss += margin
-
+        
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
-  loss /= num_train
+  loss /= num_train #除掉训练集的个数
+  dW /= num_train
+  dW += reg*W
 
   # Add regularization to the loss.
-  loss += reg * np.sum(W * W)
+  loss += reg * np.sum(W * W) #加上正则项的惩罚，在这里我们采用的是L2范式，L2范式通过对每一个参数进行平方后进行相加，通过这样的方式来抑制参数不要太大
+  # 有些地方好像写的是0.5*reg*np.sum(W*W) #我看有个知乎评论是这样的：这个应该是看regulation loss 如何定义，通常加入0.5是为了计算regulation loss的梯度时系数变为1
 
   #############################################################################
   # TODO:                                                                     #
@@ -50,7 +58,7 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
+  #接下来是要计算损失函数的梯度并把它存储到dW里，按照这段的说法是我们直接在上面计算偏导数!
 
   return loss, dW
 
@@ -60,16 +68,60 @@ def svm_loss_vectorized(W, X, y, reg):
   Structured SVM loss function, vectorized implementation.
 
   Inputs and outputs are the same as svm_loss_naive.
+  W: 3073*10
+  X: 500*3073
+  y: 500,
+  reg: 常数，0.000005
   """
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
+  #print('--------------------------')
+  #print(X.shape)
+  #print(W.shape)
+  #print(y.shape)
+  #print(X.dot(W))
+  #print(y)
+  #print('--------------------------')
+  scores = X.dot(W) # shape: 500*10, 即每一个样本对于每一个种类都有一个得分
+  correct_class_scores = scores[np.arange(num_train), y] #shape: 500,
+  #print(correct_class_scores.shape) 
+  #print(correct_class_scores) 
+  correct_class_scores = np.reshape(correct_class_scores, (-1, 1))
+  #print(correct_class_scores.shape) 
+  #print(correct_class_scores)
+  #print(scores.shape)
+  #print("**************************")
+  #print(y.shape)
+  #print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+  margin = scores - correct_class_scores + 1 #(500,10)
+  #print(margin)
+  #print("&&&&&&&&&&&&&&&&&&&&&&&&&&")
+  margin[np.arange(num_train), y] = 0 #把正确标签的偏差值置为0
+  #print(margin)
+  #print("@@@@@@@@@@@@@@@@@@@@@@@@@@")
+  margin[margin < 0] = 0
+  loss = np.sum(margin)
+  loss /= num_train
+  loss += reg * np.sum(W * W)
+  
+  margin[margin>0] = 1
+  one_num_per_line = np.sum(margin, axis = 1) #统计每一行里面的1的和（其实也就是1的个数）
+  #print(one_num_per_line)
+  #print(one_num_per_line.shape)
+  margin[np.arange(num_train), y] = -one_num_per_line
+  dW = X.T.dot(margin)
+  dW /= num_train
+  dW += reg*W
+  
+  
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
